62819b8f0ef023493a9cb2a6bb9ff8e0970a11f6
eagan76 <hydrogeneag@gmail.com>
2025-10-04 13:31:58 -0500

    Change ins.sh to installer.sh and made it better overall addert v2.0.4

--- ins.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Now installing addert when done you can run addert to use it!"
-sudo mv ~/addert/bin/addert /bin
-echo "DONE!"
---
+++ installer.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo -e "\033[0;34mInstalling Addert\033[0m"
+sudo cp ~/addert/bin/addert /bin
+echo -e "\033[0;32mInstall Complete\033[0m"
