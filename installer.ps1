ffa4977c2028d4906e575c0f58723133a6843dc2
eagan76 <101474315+eagan76@users.noreply.github.com>
2025-10-12 16:53:05 -0500

    Delete installer.ps1

--- installer.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "Installing Addert" -ForegroundColor Blue
-Set-Alias addert.exe "$env:USERPROFILE\addert\bin\addert.exe"
-Set-Alias run_addert.bat "$env:USERPROFILE\addert\bin\run_addert.bat"
-Write-Host "Install Finished" -ForgroundColor Green
