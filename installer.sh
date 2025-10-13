300c4f49cb7e17bf18fd833c4db5798ce36e3110
eagan76 <hydrogeneag@gmail.com>
2025-10-13 13:59:25 -0500

    Added sleep to install to make it better

--- installer.sh
+++ installer.sh
@@ -5,26 +5,36 @@
 
 clear
 echo -e "Starting Install...\n"
-echo -e "\033[0;34mInstalling Addert_Linux v2\n\033[0m"
-echo -e "Installing localclock_loop.sh"
+sleep 1
+echo -e "\033[0;34mInstalling Addert_Linux v2.1\n\033[0m"
+sleep 0.5
+echo -e "Installing localclock_loop.sh..."
+sleep 1
 sudo cp ~/addert/bin/localclock_loop.sh /bin
-echo -e "Done"
-echo -e "Installing localclock"
+echo -e "Done\n"
+echo -e "Installing localclock..."
+sleep 1
 sudo cp ~/addert/bin/localclock /bin
-echo -e "Done"
-echo -e "Installing clockloop.sh"
+echo -e "Done\n"
+echo -e "Installing clockloop.sh..."
+sleep 1
 sudo cp ~/addert/bin/clockloop.sh /bin
-echo -e "Done"
-echo -e "Installing addert"
+echo -e "Done\n"
+echo -e "Installing addert..."
+sleep 1
 sudo cp ~/addert/bin/addert /bin
-echo -e "Done"
-echo -e "Installing run_addert.sh"
+echo -e "Done\n"
+echo -e "Installing run_addert.sh..."
+sleep 1
 sudo cp ~/addert/bin/run_addert.sh /bin
-echo -e "Done"
-echo -e "Installing clock.sh"
+echo -e "Done\n"
+echo -e "Installing clock.sh..."
+sleep 1
 sudo cp ~/addert/bin/clock.sh /bin
-echo -e "Done"
-echo -e "Installing clock"
+echo -e "Done\n"
+echo -e "Installing clock..."
+sleep 1
 sudo cp ~/addert/bin/clock /bin
 echo -e "Done\n"
+sleep 1
 echo -e "\033[0;32mInstall Complete!\033[0m"
